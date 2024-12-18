ea950c2c9aa5de57af4d81fdfb6208aa152a85e0
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2024-12-18 13:59:44 +0200

    feat: frontend working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const path = require("path");
 
 let counter = 3;
 
@@ -10,6 +11,8 @@ let db = [
 ];
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // http get
 // http://localhost:3000/customers
